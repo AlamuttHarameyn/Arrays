788f864fd9bfffec6c992510d1ca3d17154aa235
Gizem <gizemustahuseyin123@gmail.com>
2022-12-30 23:06:34 +0300

    Add files via upload

---
+++ Arrays.cs
@@ -0,0 +1,26 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3];
+            {
+                students[0] = "Gizem";
+                students[1] = "Mehmet";
+                students[2] = "Güven";
+
+                string[] students2 = new[] {"Gizem","Mehmet","Güven" };  //Diziler yukarıdaki gibi satır satır da bu şekilde yanyana da gösterilebilir..
+
+                foreach (var student in students) //Dizinin tüm elemanlarını görmek için kullanılır
+                {
+                    Console.WriteLine(student);
+                }
+
+                Console.WriteLine();
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
